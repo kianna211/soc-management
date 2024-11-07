74c5e135a33b80fd8de85cde6b944edb19821791
Muhammad A <hotuanminh7268@gmail.com>
2024-11-07 13:49:38 +0500

    Add Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 
 const App = () => {
-	return <Header />;
+	return (
+		<>
+			<Header />
+			<Outlet />
+		</>
+	);
 };
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,46 +1,51 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
 	return (
 		<nav className="navbar navbar-expand-lg bg-body-tertiary shadow-sm">
 			<div className="container-fluid px-5 py-2">
-				<a className="navbar-brand d-flex fs-4 fw-bold fst-italic gap-3 align-items-center" href="#">
+				<Link className="navbar-brand d-flex fs-4 fw-bold fst-italic gap-3 align-items-center" to="/">
 					<img src="../../public/logo.png" alt="√" width={160} height={160} style={{ width: "45px", height: "45px" }} />
 					Soc effective
-				</a>
+				</Link>
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 				</button>
 				<div className="collapse navbar-collapse" id="navbarNav">
-					<ul className="ms-auto navbar-nav gap-3">
-						<li className="nav-item">
-							<a className="nav-link active" aria-current="page" href="#">
+					<ul className="ms-auto navbar-nav gap-3 align-items-center">
+						<div className="nav-item">
+							<Link className="nav-link active" aria-current="page" to="/">
 								Home
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">
-								Assignment
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">
+							</Link>
+						</div>
+						<div className="nav-item">
+							<Link className="nav-link" to="/assess">
+								Assessment
+							</Link>
+						</div>
+						<div className="nav-item">
+							<Link className="nav-link" to="/results">
 								Results
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" aria-disabled="true">
+							</Link>
+						</div>
+						<div className="nav-item">
+							<Link className="nav-link" to="/guide">
 								How to
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" aria-disabled="true">
+							</Link>
+						</div>
+						<div className="nav-item">
+							<Link className="nav-link" to="/contact">
 								Contact Us
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" aria-disabled="true">
-								Login
-							</a>
-						</li>
+							</Link>
+						</div>
+						<div className="nav-item d-flex gap-0">
+							<Link className="nav-link" to="/login">
+								<button className="btn btn-outline-success text-white">Login</button>
+							</Link>
+							<Link className="nav-link" to="/signup">
+								<button className="btn btn-outline-success text-white">Sign Up</button>
+							</Link>
+						</div>
 					</ul>
 				</div>
 			</div>
