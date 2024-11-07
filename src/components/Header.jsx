14920cd55b9c3f7d77fb4a9f9b5f539be3cbcc28
Muhammad A <hotuanminh7268@gmail.com>
2024-11-07 20:32:14 +0500

    Modify Router

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
+const menuItems = {
+	Home: "/",
+	Assessment: "/assess",
+	Results: "/result",
+	"How to": "/guide",
+	"Contact Us": "/contact",
+};
 
 const Header = () => {
+	const location = useLocation();
+
 	return (
 		<nav className="navbar navbar-expand-lg bg-body-tertiary shadow-sm">
 			<div className="container-fluid px-5 py-2">
 				<Link className="navbar-brand d-flex fs-4 fw-bold fst-italic gap-3 align-items-center" to="/">
-					<img src="../../public/logo.png" alt="√" width={160} height={160} style={{ width: "45px", height: "45px" }} />
+					<img src="/logo.png" alt="√" width={160} height={160} style={{ width: "45px", height: "45px" }} />
 					Soc effective
 				</Link>
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,39 +23,22 @@ const Header = () => {
 				</button>
 				<div className="collapse navbar-collapse" id="navbarNav">
 					<ul className="ms-auto navbar-nav gap-3 align-items-center">
-						<div className="nav-item">
-							<Link className="nav-link active" aria-current="page" to="/">
-								Home
+						{Object.keys(menuItems).map((item, index) => (
+							<li className={`nav-item ${location.pathname === menuItems[item] ? "active" : ""}`} key={index}>
+								<Link
+									className={`nav-link ${location.pathname === menuItems[item] ? "active" : ""}`}
+									aria-current={location.pathname === menuItems[item] ? "page" : undefined}
+									to={".." + menuItems[item]}
+								>
+									{item}
+								</Link>
+							</li>
+						))}
+						<li className="nav-item d-flex gap-0">
+							<Link className="nav-link" to="/auth">
+								<button className={`btn text-white py-2 px-3 ${location.pathname == "/auth" ? "btn-primary" : "btn-outline-primary"}`}>Get Started</button>
 							</Link>
-						</div>
-						<div className="nav-item">
-							<Link className="nav-link" to="/assess">
-								Assessment
-							</Link>
-						</div>
-						<div className="nav-item">
-							<Link className="nav-link" to="/results">
-								Results
-							</Link>
-						</div>
-						<div className="nav-item">
-							<Link className="nav-link" to="/guide">
-								How to
-							</Link>
-						</div>
-						<div className="nav-item">
-							<Link className="nav-link" to="/contact">
-								Contact Us
-							</Link>
-						</div>
-						<div className="nav-item d-flex gap-0">
-							<Link className="nav-link" to="/login">
-								<button className="btn btn-outline-success text-white">Login</button>
-							</Link>
-							<Link className="nav-link" to="/signup">
-								<button className="btn btn-outline-success text-white">Sign Up</button>
-							</Link>
-						</div>
+						</li>
 					</ul>
 				</div>
 			</div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,22 +4,43 @@ import App from "./App.jsx";
 import "./assets/scss/style.scss";
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home.jsx";
-import Login from "./pages/Login.jsx";
-import Signup from "./pages/Signup.jsx";
+import Auth from "./pages/Auth.jsx";
+import Assessment from "./pages/Assessment.jsx";
+import Results from "./pages/Results.jsx";
+import Guide from "./pages/Guide.jsx";
+import Contact from "./pages/Contact.jsx";
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path="/" element={<App />}>
 			<Route path="/" element={<Home />} />
-			<Route path="/login" element={<Login />} />
-			<Route path="/signup" element={<Signup />} />
+			<Route path="/assess" element={<Assessment />} />
+			<Route path="/result" element={<Results />} />
+			<Route path="/guide" element={<Guide />} />
+			<Route path="/contact" element={<Contact />} />
+			<Route path="/auth" element={<Auth />} />
 		</Route>
-	)
+	),
+	{
+		future: {
+			v7_relativeSplatPath: true,
+			v7_fetcherPersist: true,
+			v7_normalizeFormMethod: true,
+			v7_partialHydration: true,
+			v7_skipActionStatusRevalidation: true,
+			v7_skipActionErrorRevalidation: true,
+		},
+	}
 );
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<RouterProvider router={router}>
+		<RouterProvider
+			router={router}
+			future={{
+				v7_startTransition: true,
+			}}
+		>
 			<App />
 		</RouterProvider>
 	</StrictMode>
